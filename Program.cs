d6d68370fa01f78cf4917140516afe450c269812
DAVe3283 <DAVe3283@gmail.com>
2015-03-20 00:05:08 -0600

    Now powers off monitors!

--- Program.cs
+++ Program.cs
@@ -1,22 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace Sleepytime
 {
     class Program
     {
         #region DLL Imports
-        [DllImport("user32")]
-        public static extern void LockWorkStation();
+        /// <summary>
+        /// Lock the workstation.
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/aa376875%28v=vs.85%29.aspx
+        /// </summary>
+        /// <returns>Nonzero means success</returns>
+        [DllImport("user32.dll")]
+        private static extern Int32 LockWorkStation();
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms644950%28v=vs.85%29.aspx
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(
+            IntPtr hWnd,
+            UInt32 Msg,
+            IntPtr wParam,
+            IntPtr lParam
+        );
         #endregion DLL Imports
 
-        static void Main(string[] args)
+        // Things that would be in a C header somewhere
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646360%28v=vs.85%29.aspx
+        private const UInt32 WM_SYSCOMMAND = 0x0112;
+        private static IntPtr SC_MONITORPOWER = (IntPtr)0xF170;
+        private static IntPtr SC_MONITORPOWER_Off = (IntPtr)2;
+
+        static int Main(string[] args)
         {
-            LockWorkStation();
-            // TODO: powersave monitors
+            // Lock the workstation
+            if (LockWorkStation() == 0)
+            {
+                MessageBox.Show(
+                    "Unable to lock the workstation!\nLockWorkStation() API call failed.",
+                    "Can't lock PC",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop);
+                return 1;
+            }
+
+            // Put monitors into powersave mode
+            using (Form tempForm = new Form())
+            {
+                SendMessage(tempForm.Handle, WM_SYSCOMMAND, SC_MONITORPOWER, SC_MONITORPOWER_Off);
+            }
+
+            // Done! Now get to sleep!
+            return 0;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 
 A simple program to lock your workstation and put all monitors into powersave
 mode.
+
+## Doesn't NirCmd do this?
+
+Yes, with a very simple nircmd script.
+
+## So why write your own?
+
+Because NirCmd hangs after putting my monitor to sleep, leaving an army of
+zombie processes over time. This annoys me more than it should.
+
+Plus, I just like writing C#, so why not? It's only a few lines of code.
+
+## What causes that?
+
+Interestingly enough, the Windows SendMessage API is not returning on my system
+when called with `HWND_BROADCAST` or `HWND_TOPMOST` and the monitor power
+command.
+
+Rather than figure out which program/driver/whatever is causing that problem, I
+just decided to write my own that doesn't broadcast the monitor off message to
+_everyone_. Instead, I create a dummy form, and send the message to that.
+
+Windows intercepts the message and does its thing, and we're golden!
